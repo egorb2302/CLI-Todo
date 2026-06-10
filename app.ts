e425bfcb07911f6e0ec462bd256d80b3a970ceba
href <berendeevegor@gmail.com>
2026-06-10 21:39:12 +0400

    fix gitignore

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 import { fileReader, Greeting, Help } from './func/func.js'
+import type { Task, CommandEvent } from './types/types.ts'
 
-const Task = require('./types/types.ts')
-const CommandEvent = require('./types/types.ts')
 const path = require('path')
 const fs = require('fs/promises')
 const readline = require('readline')
@@ -18,14 +17,14 @@ const createTask = async (title: string, description: string, isCompleted: boole
 
     try {
         const folderPath: string = path.resolve(__dirname, "tasks", `task${uniqueID}.json`);
-        const task: typeof Task = {
+        const task: Task = {
             id: uniqueID,
             title: title,
             isCompleted: isCompleted,
             description: description
         }
 
-        await fs.promises.writeFile(folderPath, JSON.stringify(task))
+        await fs.writeFile(folderPath, JSON.stringify(task))
         console.log("Successfuly created a task!")
     } catch (err) {
         console.error(err)
@@ -37,8 +36,8 @@ const removeTask = async (id: string): Promise<void> => {
 
 }
 
-const getTasks = async (): Promise<undefined | void> => {
-    const tasks: typeof Task[] = []; 
+const getTasks = async (): Promise<undefined | Task[]> => {
+    const tasks: Task[] = []; 
 
     try {
         const folderPath: string = path.resolve(__dirname, "tasks")
@@ -60,9 +59,11 @@ const getTasks = async (): Promise<undefined | void> => {
     tasks.forEach((task) => {
         console.log(`Task ${(tasks.indexOf(task)) + 1}: ${chalk.green(task.title)}`)
     })
+
+    return tasks.sort((a, b) => b.id - a.id);
 }
 
-const CommandHandler = async (input: typeof CommandEvent): Promise<void> => {
+const CommandHandler = async (input: CommandEvent): Promise<void> => {
     const { command } = input;
 
     switch(command) {
